db09d30b164a4f54c2c1bb42980d466f3574c154
fmattera <f.mattera@student.vu.nl>
2024-11-24 12:37:56 +0100

    fix: name

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,10 @@
 const Footer = () => {
   return (
-    <footer className='mb-8 mt-16 flex justify-center'>
-      &copy; {new Date().getFullYear()} Francesco Mattera
+    <footer className='mb-8 mt-16 flex flex-col items-center text-center'>
+      <div className='text-m text-gray-500'>
+        Built by <span className='font-medium text-white'>Francesco Mattera</span>.
+      </div>
+      <div className='mt-2 text-xs text-gray-500'>&copy; {new Date().getFullYear()}</div>
     </footer>
   )
 }
